1b0577e583d9cf328f8c092147612d6f6e5abd9d
Cameron Dubas <camerondubas@gmail.com>
2019-10-08 21:51:40 -0700

    Solution for reverse-string

---
+++ reverse-string/src/lib.rs
@@ -0,0 +1,11 @@
+extern crate unicode_segmentation;
+
+use unicode_segmentation::UnicodeSegmentation;
+
+pub fn reverse(input: &str) -> String {
+    // // Basic Solution (no dependecies)
+    // input.chars().rev().collect()
+
+    // Bonus Solution (unicode-segmentation)
+    input.graphemes(true).rev().collect()
+}
