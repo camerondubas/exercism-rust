7968b2eaaa208a3e9d21abca6e1177ebe0c2f9d0
Cameron Dubas <camerondubas@gmail.com>
2019-10-09 17:38:28 -0700

    Solution for nth-prime

---
+++ nth-prime/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn nth(n: u32) -> u32 {
+    let mut prime_numbers: Vec<u32> = Vec::new();
+
+    let mut num = 2;
+
+    while (prime_numbers.len() as u32) < n + 1 {
+      if is_prime(num) {
+        println!("Found Prime: {}", num);
+        prime_numbers.push(num)
+      }
+
+      num = num + 1;
+    }
+
+    prime_numbers.pop().expect("Error")
+}
+
+fn is_prime(num: u32) -> bool {
+  let largest_factor = num / 2;
+  let lowest_factor = 2;
+
+  let mut inc = lowest_factor;
+
+  while inc <= largest_factor {
+    if num % inc == 0 {
+      return false;
+    }
+
+    inc = inc + 1;
+  }
+
+  true
+}
