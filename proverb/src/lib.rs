2c613776ce59186b63ef40b6e4fcf41314535445
Cameron Dubas <camerondubas@gmail.com>
2019-10-12 12:02:23 -0700

    Add solution for proverb

---
+++ proverb/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    let mut output = String::from("");
+    let mut iter = list.iter().peekable();
+
+    while let Some(current) = iter.next() {
+      let next = iter.peek();
+
+      let line = match next {
+        Some(_x) => format!("For want of a {} the {} was lost.\n", current, next.unwrap()),
+        None => format!("And all for the want of a {}.", list.iter().nth(0).unwrap())
+      };
+
+      output = format!("{}{}", output, line)
+    }
+
+    output
+}
