c00c5443298c0f867e2edcf53e97900a00fb2c70
Cameron Dubas <camerondubas@gmail.com>
2019-10-13 23:04:07 -0700

    Add link to formula

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,4 +1,6 @@
 pub fn square(number: u32) -> u64 {
+    // nth term in doubling sequence:
+    // https://socratic.org/questions/how-do-you-find-the-nth-term-rule-for-1-2-4-8-16-32-64
     if number == 0 || number > 64 {
       panic!("Square must be between 1 and 64");
     }
@@ -6,7 +8,7 @@ pub fn square(number: u32) -> u64 {
     let first_term: u64 = 1;
     let ratio: u64 = 2;
 
-    (first_term * ratio).pow(number - 1);
+    (first_term * ratio).pow(number - 1)
 }
 
 pub fn total() -> u64 {
