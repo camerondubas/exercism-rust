475ba438140f91de6b8bbc59d849ed239e1d660d
Cameron Dubas <camerondubas@gmail.com>
2019-10-14 01:28:19 -0700

    update solution for readability

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -2,8 +2,9 @@ pub fn factors(n: u64) -> Vec<u64> {
     let mut factors = Vec::new();
     let mut num = n;
 
-     while num > 1 {
-        let factor = (2..num+1).find(|x| num % x == 0 ).unwrap();
+    while num > 1 {
+        let mut range = 2..=num;
+        let factor = range.find(|x| num % x == 0).unwrap();
         factors.push(factor);
 
         num = num / factor;
