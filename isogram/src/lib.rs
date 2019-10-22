3064453c08b5b760bd12bbe59d6249278813b455
Cameron Dubas <camerondubas@gmail.com>
2019-10-21 23:00:25 -0700

    remove extra var

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -2,7 +2,6 @@ use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
     let mut used_chars = HashSet::new();
-    let is_valid = true;
 
     for c in candidate.to_lowercase().chars() {
       if c.is_alphabetic() {
@@ -14,5 +13,5 @@ pub fn check(candidate: &str) -> bool {
       }
     }
 
-  is_valid
+  true
 }
