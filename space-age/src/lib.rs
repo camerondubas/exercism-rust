248954a5fc17937899936bb4f97a3f2edc8a76b6
Cameron Dubas <camerondubas@gmail.com>
2019-11-25 23:35:53 -0800

    Add solution for space-age

---
+++ space-age/src/lib.rs
@@ -0,0 +1,67 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(Debug)]
+pub struct Duration {
+    years: f64,
+}
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        const SECONDS: f64 = 60.;
+        const MINUTES: f64 = 60.;
+        const HOURS: f64 = 24.;
+        const DAYS: f64 = 365.25;
+
+        Duration {
+            years: s as f64 / (SECONDS * MINUTES * HOURS * DAYS),
+        }
+    }
+}
+
+pub trait Planet {
+    const EARTH_YEARS: f64 = 1.;
+
+    fn years_during(d: &Duration) -> f64 {
+        d.years / Self::EARTH_YEARS
+    }
+}
+
+pub struct Mercury;
+pub struct Venus;
+pub struct Earth;
+pub struct Mars;
+pub struct Jupiter;
+pub struct Saturn;
+pub struct Uranus;
+pub struct Neptune;
+
+impl Planet for Mercury {
+    const EARTH_YEARS: f64 = 0.2408467;
+}
+
+impl Planet for Venus {
+    const EARTH_YEARS: f64 = 0.61519726;
+}
+
+impl Planet for Earth {}
+
+impl Planet for Mars {
+    const EARTH_YEARS: f64 = 1.8808158;
+}
+
+impl Planet for Jupiter {
+    const EARTH_YEARS: f64 = 11.862615;
+}
+
+impl Planet for Saturn {
+    const EARTH_YEARS: f64 = 29.447498;
+}
+
+impl Planet for Uranus {
+    const EARTH_YEARS: f64 = 84.016846;
+}
+
+impl Planet for Neptune {
+    const EARTH_YEARS: f64 = 164.79132;
+}
